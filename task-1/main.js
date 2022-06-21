ad820675df1fc6346b0b139d757daa1196b8777c
AgalarovBaycan <106188929+AgalarovBaycan@users.noreply.github.com>
2022-06-21 19:24:00 +0400

    Update main.js

--- task-1/main.js
+++ task-1/main.js
@@ -1,24 +1,9 @@
-const prompt = require('prompt');
-prompt.start();
+let n = 55
+let last = n % 10
+let first = parseInt(n / 10)
 
-
-prompt.get('input', function (err, result) {
-    /* 
-    Istifadəçinin daxil etdiyi dəyişəni qəbul edirik. 
-    Giriş veriləni String type olur. Number type-a çevirmək üçün parseInt() metodundan istifadə edirik.
-    Vergüllə daxil edilmiş sətri iki fərqli ədədə bölmək üçün split metodundan istifadə edirik.
-    */
-    var input = result.input; // "2,3,5"
-    var numbers = input.split(','); // ['2', '3', '5']
-    var n = parseInt(numbers[0]); // Ilk daxil olunmuş ədədi alırıq. Nümunə - 2 
-    var last = parseInt(numbers[1]); // İkinci daxil olunmuş ədədi alırıq. Nümunə - 3
-    var first = parseInt(numbers[2]);
-    let n = 55
-    if(n<100){
-        let last = n % 10
-    let first = parseInt(n / 10)
-        console.log(last + first)
-    }
-    
-    
-});
+if(n<100){
+    let last = n % 10
+let first = parseInt(n / 10)
+    console.log(last + first)
+}
