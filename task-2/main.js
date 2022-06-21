ed7b48f95f8ca49a5c922e9e097ca020cdc771dc
AgalarovBaycan <106188929+AgalarovBaycan@users.noreply.github.com>
2022-06-21 20:52:39 +0400

    Update main.js

--- task-2/main.js
+++ task-2/main.js
@@ -1,9 +1,36 @@
-const prompt = require('prompt');
-prompt.start();
+let n = 327
+let last= n % 10
+let first = parseInt(n/100)
+let second= parseInt(n/10)%10
+let max = 0
+let mid =0
+let end = 0
 
 
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+if(last>second && last > first){
+    max = last
+}else if(second>first && second>last){
+    max= second
+}else if (first > second && first > last ){
+    max = first
+}
+
+if(first>second){
+    mid=first
+}else if (second>first){
+    mid = second
+}
+
+if(second<mid && second<max){
+    end =second
+}else if(first<mid&&first<max){
+    end = first
+}else if(last < mid&& last<max){
+    end = last
+}
+
+
+const number = (""+max +mid +end)
+
+const kvadrat = number*number
+console.log(kvadrat)
