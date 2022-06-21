64f193ddaa989bfe9fd979ef2607c3961aa10c8b
AgalarovBaycan <106188929+AgalarovBaycan@users.noreply.github.com>
2022-06-21 20:37:07 +0400

    Update main.js

--- task-3/main.js
+++ task-3/main.js
@@ -1,9 +1,20 @@
-const prompt = require('prompt');
-prompt.start();
-
-
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+let n = 63456
+let max = 0
+let count = 0
+while(n > 0){
+    const num = n % 10
+    if(num > max){
+        max = num
+        count = 1
+    }
+    else if(num == max){
+        count++
+    }
+    n = parseInt( n / 10 )
+}
+if (count >= 2){
+    console.log("YES");
+}
+else{
+    console.log("NO");
+}
